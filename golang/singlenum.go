f7d999819c4dbba9c11c9cac172fd06b0b0f0bea
xiheng <15201732653@163.com>
2025-06-24 06:17:53 +0800

    提交只有一个数字

---
+++ golang/singlenum.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func singleNumber(nums []int) int {
+    single := 0
+    for _, num := range nums {
+        single ^= num
+    }
+    return single
+}
+
+func main() {
+    var nums = []int{2, 2, 3, 3, 5}
+    num := singleNumber(nums)
+
+    fmt.Println("只出现一个数字：",num)
+}
